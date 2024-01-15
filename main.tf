091bfc31a0d46d0356a49aff434b2a30b19d29de
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-15 17:33:01 +0100

    implement NACL
    this was interesting, we create an ACL
    
    then to it we add specific rules as their own resources
    
    then we have another resource aws_network_acl_association that associates the acl with the subnet

--- main.tf
+++ main.tf
@@ -32,6 +32,57 @@ resource "aws_internet_gateway" "example_igw" {
   vpc_id = aws_vpc.example_vpc.id
 }
 
+# Create a new network ACL
+resource "aws_network_acl" "example_nacl" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  tags = {
+    Name = "example_nacl"
+  }
+}
+
+# Add inbound rules to the NACL
+resource "aws_network_acl_rule" "inbound_http" {
+  network_acl_id = aws_network_acl.example_nacl.id
+  rule_number    = 100
+  rule_action    = "allow"
+  egress         = false
+  protocol       = "tcp"
+  from_port      = 80
+  to_port        = 80
+  cidr_block     = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "inbound_ssh" {
+  network_acl_id = aws_network_acl.example_nacl.id
+  rule_number    = 200
+  rule_action    = "allow"
+  egress         = false
+  protocol       = "tcp"
+  from_port      = 22
+  to_port        = 22
+  cidr_block     = "0.0.0.0/0"
+}
+
+# Add outbound rule to the NACL
+resource "aws_network_acl_rule" "outbound_all" {
+  network_acl_id = aws_network_acl.example_nacl.id
+  rule_number    = 300
+  rule_action    = "allow"
+  egress         = true
+  protocol       = "-1"
+  from_port      = 0
+  to_port        = 0
+  cidr_block     = "0.0.0.0/0"
+}
+
+# Associate the NACL with the subnet
+resource "aws_network_acl_association" "example_nacl_association" {
+  network_acl_id = aws_network_acl.example_nacl.id
+  subnet_id      = aws_subnet.example_subnet.id
+}
+
+
 
 resource "aws_subnet" "example_subnet" {
   vpc_id            = aws_vpc.example_vpc.id
--- outputs.tf
+++ outputs.tf
@@ -27,3 +27,18 @@ output "security_group_name" {
   description = "The name of the security group"
   value       = aws_security_group.app_server_sg.name
 }
+
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.app_server_sg.id
+}
+
+output "nacl_id" {
+  description = "The ID of the NACL"
+  value       = aws_network_acl.example_nacl.id
+}
+
+output "nacl_name" {
+  description = "The name of the NACL"
+  value       = aws_network_acl.example_nacl.tags.Name
+}

446399c7c08203941d72388e143112df8bde02d2
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-15 10:00:47 +0100

    fix: make sure ec2 instance has public ip address

--- main.tf
+++ main.tf
@@ -37,6 +37,8 @@ resource "aws_subnet" "example_subnet" {
   vpc_id            = aws_vpc.example_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
+  // ensures all instances launched in this subnet are automatically assigned a public IP address
+  map_public_ip_on_launch = true
 }
 
 resource "aws_instance" "app_server" {
